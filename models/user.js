92553138f21bf3083e9d14ed7d5f717d2f43a42e
lakshy pachory <luckyron1279@gmail.com>
2020-05-06 16:35:31 +0530

    add contact controller

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose')
 
 
 const userSchema=new mongoose.Schema(
-    {
+{
         email:{
             type:String,
             required:true,
@@ -20,7 +20,7 @@ const userSchema=new mongoose.Schema(
     },{
         //The {timestamps: true} option creates a createdAt and updatedAt field on our models that contain timestamps which will get automatically updated when our model changes.
         timestamps:true
-    });
+});
 
 const User=mongoose.model('User',userSchema);
 module.exports=User;
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const express=require('express');
 const userController=require('../controllers/user_controller');
 const router=express.Router();
 router.use('/users',require('./user'));
+router.use('/contact',require('./contact'));
 router.get('/',userController.home);
 module.exports=router;
